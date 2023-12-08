220e35089505ad0811003789054b56dd9b5db352
Min Min Chit <min1min2chit3@gmail.com>
2023-12-08 11:39:32 +0630

    call back exercise

--- lib/screens/reserve_screen/main_reserve_screen.dart
+++ lib/screens/reserve_screen/main_reserve_screen.dart
@@ -14,7 +14,7 @@ class MainReserveScreen extends StatefulWidget {
 
 class _MainReserveScreenState extends State<MainReserveScreen> {
   final PageController pageController = PageController();
-  double lineWidth = 0 ;
+  double lineWidth = 0;
   bool isDateStep = false;
   bool isTimeStep = false;
   bool isGuestStep = false;
@@ -24,116 +24,119 @@ class _MainReserveScreenState extends State<MainReserveScreen> {
     final Size size = MediaQuery.of(context).size;
     return WillPopScope(
         onWillPop: () async {
-          BlocProvider.of<ReservationBloc>(context).add(ReservationInitialEvent());
+          BlocProvider.of<ReservationBloc>(context)
+              .add(ReservationInitialEvent());
           Navigator.pop(context);
-          return true ;
+          return true;
         },
-        child:Scaffold(
+        child: Scaffold(
           appBar: AppBar(
             title: const Text('Reserve a Table'),
           ),
           body: Column(
             children: [
               Padding(
-                  padding:
-                  const EdgeInsets.symmetric(horizontal: 20,vertical: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  GestureDetector(
-                    onTap: (){
-                      DateTime selectedDate =
-                      BlocProvider.of<ReservationBloc>(context).getReservationDate;
-                      if(selectedDate.year !=0){
-                        animatedToPage(0);
-                      }
-                    },
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            SvgPicture.asset('assets/icons/date_step.svg'),
-                            StepLineContainer(
-                                lineWidth: lineWidth, isStep: isTimeStep)
-                          ],
-                        ),
-                        const StepperText(
-                            text: 'date',
-                            isStep: true)
-                      ],
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        DateTime selectedDate =
+                            BlocProvider.of<ReservationBloc>(context)
+                                .getReservationDate;
+                        if (selectedDate.year != 0) {
+                          animatedToPage(0);
+                        }
+                      },
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              SvgPicture.asset('assets/icons/date_step.svg'),
+                              StepLineContainer(
+                                  lineWidth: lineWidth, isStep: isTimeStep)
+                            ],
+                          ),
+                          const StepperText(text: 'date', isStep: true)
+                        ],
+                      ),
                     ),
-                  ),
-                  GestureDetector(
-                    onTap: (){
-                      isTimeStep = true ;
-                      animatedToPage(1);
-                      setState(() {});
-                    },
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            isTimeStep
-                                ? SvgPicture.asset('assets/icons/time_step.svg')
-                                : SvgPicture.asset('assets/icons/time_unstep.svg'),
-                            StepLineContainer(lineWidth: lineWidth, isStep: isGuestStep)
-                          ],
-                        ),
-                        StepperText(
-                            text: 'time',
-                            isStep: isTimeStep)
-                      ],
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: (){
-                      if(isTimeStep){
-                        isGuestStep = true;
-                        animatedToPage(2);
+                    GestureDetector(
+                      onTap: () {
+                        isTimeStep = true;
+                        animatedToPage(1);
                         setState(() {});
-                      }
-                    },
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            isGuestStep
-                                ? SvgPicture.asset('assets/icons/guest_step.svg')
-                                :SvgPicture.asset('assets/icons/guest_unstep.svg'),
-                          ],
-                        ),
-                        StepperText(
-                            text: 'guest',
-                            isStep: isGuestStep)
-                      ],
+                      },
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              isTimeStep
+                                  ? SvgPicture.asset(
+                                      'assets/icons/time_step.svg')
+                                  : SvgPicture.asset(
+                                      'assets/icons/time_unstep.svg'),
+                              StepLineContainer(
+                                  lineWidth: lineWidth, isStep: isGuestStep)
+                            ],
+                          ),
+                          StepperText(text: 'time', isStep: isTimeStep)
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                    GestureDetector(
+                      onTap: () {
+                        if (isTimeStep) {
+                          isGuestStep = true;
+                          animatedToPage(2);
+                          setState(() {});
+                        }
+                      },
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              isGuestStep
+                                  ? SvgPicture.asset(
+                                      'assets/icons/guest_step.svg')
+                                  : SvgPicture.asset(
+                                      'assets/icons/guest_unstep.svg'),
+                            ],
+                          ),
+                          StepperText(text: 'guest', isStep: isGuestStep)
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              ),
-              Expanded(child: PageView(
-                controller: pageController,
-                physics: const NeverScrollableScrollPhysics(),
-                onPageChanged: (index){},
-                children: [
-                  CalendarPage(
-                    continueClick: (){
+              Expanded(
+                child: PageView(
+                  controller: pageController,
+                  physics: const NeverScrollableScrollPhysics(),
+                  onPageChanged: (index) {},
+                  children: [
+                    CalendarPage(continueClick: () {
                       stepToTime(context);
-                    }
-                  ),
-                  TimePage(
-                    continueClick: () { stepToGuest(context); },
-                    backToCalendar: () {  },
-                  ),
-                ],
-              ))
+                    }),
+                    TimePage(
+                      continueClick: () {
+                        stepToGuest(context);
+                      },
+                      backToCalendar: () {},
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
-        )
-    );
+        ));
   }
+
   void stepToTime(BuildContext context) {
     DateTime selectedDate =
         BlocProvider.of<ReservationBloc>(context).getReservationDate;
@@ -153,6 +156,7 @@ class _MainReserveScreenState extends State<MainReserveScreen> {
     isGuestStep = true;
     setState(() {});
   }
+
   void animatedToPage(int pageIndex) {
     pageController.animateToPage(
       pageIndex,
@@ -203,9 +207,7 @@ class StepperText extends StatelessWidget {
           child: FittedBox(
             child: Text(
               text,
-              style: TextStyle(
-                color: isStep ? Colors.black38: Colors.white
-              ),
+              style: TextStyle(color: isStep ? Colors.black38 : Colors.white),
             ),
           ),
         ),
